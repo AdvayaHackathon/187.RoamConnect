74fb8401bb13605a05e7fd62d0f9ff1c7b2d020b
thinker010 <punithreddyv2004@gmail.com>
2025-04-12 01:24:15 +0530

    changed to production

--- server.py
+++ server.py
@@ -635,4 +635,4 @@ def delete_emergency_contact(contact_id):
         conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
